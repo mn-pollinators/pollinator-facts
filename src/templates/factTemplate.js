a4d1150dd83e0aa576d4409703506831aeb2c0c7
Paul Friederichsen <floogulinc@gmail.com>
2020-03-11 03:07:37 -0500

    Add SEO title in fact template

--- src/templates/factTemplate.js
+++ src/templates/factTemplate.js
@@ -2,11 +2,13 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Img from "gatsby-image"
+import SEO from "../components/seo"
 
 export default ({ data }) => {
   const fact = data.markdownRemark
   return (
     <Layout>
+      <SEO title={fact.frontmatter.title} />
       <div>
         <h1>{fact.frontmatter.title}</h1>
         <Img fluid={fact.frontmatter.image.src.childImageSharp.fluid}/>
