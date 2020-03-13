838f62a154419393f677cc31840d27476d47f992
Paul Friederichsen <floogulinc@gmail.com>
2020-03-12 23:31:23 -0500

    Work on Material UI theme stuff

--- src/components/large-fact-card.js
+++ src/components/large-fact-card.js
@@ -3,19 +3,18 @@ import Img from "gatsby-image"
 
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import { Button } from 'gatsby-theme-material-ui';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
     root: {
-      maxWidth: 345,
+      maxWidth: 520,
     },
     media: {
-      height: 180,
+      height: 250,
     },
   });
 
@@ -45,12 +44,12 @@ export default function LargeFactCard({factTitle, factImage, factSource, factHTM
             </CardContent>
 
             <CardActions>
-            <Button size="small" color="primary">
-                Share
-            </Button>
-            <Button size="small" color="primary">
-                Learn More
-            </Button>
+                <Button size="small" color="primary">
+                    Share
+                </Button>
+                <Button size="small" color="primary">
+                    Learn More
+                </Button>
             </CardActions>
         </Card>
     )
--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -1,9 +1,15 @@
 import { createMuiTheme } from "@material-ui/core";
+import green from '@material-ui/core/colors/green';
+import amber from '@material-ui/core/colors/amber';
 
 const theme = createMuiTheme({
   //   palette: {
   //     type: 'dark',
   //   },
+  palette: {
+    primary: green,
+    secondary: amber,
+  },
 });
 
 export default theme;
