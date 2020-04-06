88c205fe8beb580ae46255bab95f28fd678f4499
Paul Friederichsen <floogulinc@gmail.com>
2020-04-06 09:21:41 -0500

    Add basic appbar to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,13 +8,30 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Header from "./header"
 import "./layout.css"
 
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  }
+}));
+
 const Layout = ({ children }) => {
+  const classes = useStyles();
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -27,6 +44,16 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+          </IconButton>
+          <Typography Typography variant="h6">
+            {data.site.siteMetadata.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Container maxWidth="sm">
         <main>{children}</main>
       </Container>
