51ff125d5ffdd5cc58cc8a1ca02885beee0bb978
Paul Friederichsen <floogulinc@gmail.com>
2020-05-11 07:24:48 -0500

    Use a nice Material list for the facts list

--- src/pages/facts.js
+++ src/pages/facts.js
@@ -2,7 +2,15 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Link } from "gatsby-theme-material-ui"
+import { GatsbyLink } from "gatsby-theme-material-ui"
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Img from "gatsby-image"
+import Typography from '@material-ui/core/Typography';
+
 
 const Facts = ({
   data: {
@@ -12,17 +20,23 @@ const Facts = ({
   return (
   <Layout>
     <SEO title="All Facts" />
-    <ul>
+    <Typography variant="h4" component="h1">
+          Facts
+    </Typography>
+    <List component="nav">
         {edges.map(({ node }) => {
           const { slug } = node.fields
-          const { title } = node.frontmatter
+          const { title, image } = node.frontmatter
           return (
-            <li key={slug}>
-              <Link to={slug}>{title}</Link>
-            </li>
+            <ListItem button component={GatsbyLink} to={slug} key={node.id}>
+              <ListItemAvatar>
+                <Avatar variant="rounded" fixed={image.src.childImageSharp.fixed} component={Img} />
+              </ListItemAvatar>
+              <ListItemText primary={title} secondary={node.excerpt}/>
+            </ListItem>
           )
         })}
-      </ul>
+    </List>
   </Layout>
   )
 }
@@ -35,12 +49,21 @@ export const pageQuery = graphql`
       edges {
         node {
           id
-          excerpt(pruneLength: 250)
+          excerpt(pruneLength: 50)
           fields {
             slug
           }
           frontmatter {
             title
+            image {
+              src {
+                  childImageSharp {
+                    fixed(width: 40, height: 40) {
+                      ...GatsbyImageSharpFixed_withWebp
+                    }
+                  }
+              }
+            }
           }
         }
       }
