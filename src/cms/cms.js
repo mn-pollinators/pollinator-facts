d43350fb708628b334494006b82a58daaf1f62a5
Paul Friederichsen <floogulinc@gmail.com>
2020-05-26 03:50:55 -0500

    Use correct data for preview fact and apply styles

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,34 +1,47 @@
 import React from 'react'
 import CMS from "netlify-cms-app"
-import { LargeFactCard } from "../components/large-fact-card"
+import LargeFactCard from "../components/large-fact-card"
 import { makeStyles } from '@material-ui/core/styles';
+import injectStyle from './injectStyle';
 
-
-const useStyles = makeStyles({
-    media: {
-      height: 250,
-    },
-    overline: {
-      lineHeight: 0,
-    },
-    body: {
-     "& p": {
-      margin: 0
-     }
-    }
+const factPageStyles = makeStyles({
+  card: {
+    maxWidth: 520,
+    marginTop: 20,
+    margin: 'auto',
+  },
 });
 
-const classes = useStyles();
+const FactPagePreview = ({ entry, widgetFor, getAsset }) => {
 
-const FactPagePreview = ({ entry, widgetFor }) => (
-    <LargeFactCard 
-    className={classes.card} 
-    factTitle={entry.getIn('data','title')}
-    factImage={entry.getIn('data','image')}
-    factSource={entry.getIn('data','source')}
-    factCategory={entry.getIn('data','category')}
-    factHTML={widgetFor('body')}
-    />
-)
+  const classes = factPageStyles();
 
-CMS.registerPreviewTemplate('facts', FactPagePreview)
+  const factImg = {
+    src: {
+      childImageSharp: {
+        fluid: {
+          src: getAsset(entry.getIn(['data','image','src']))
+        }
+      },
+      alt: entry.getIn(['data','image','alt']),
+      creator: entry.getIn(['data','image','creator']),
+      license: entry.getIn(['data','image','license']),
+      url: entry.getIn(['data','image','url'])
+    }
+  }
+
+  return (
+    <>
+          <LargeFactCard
+          className={classes.card}
+          factTitle={entry.getIn(['data','title'])}
+          factImage={factImg}
+          factSource={entry.getIn(['data','source'])}
+          factCategory={entry.getIn(['data','category'])}
+          factHTML={entry.getIn(['data','body'])}
+          />
+    </>
+  )
+}
+
+CMS.registerPreviewTemplate('facts', injectStyle(FactPagePreview));
