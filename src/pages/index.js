204f2b03690ab0cacf797915638e3809bace617f
Utkarsh Kumar <kumar375@morris.umn.edu>
2020-05-27 15:36:52 -0500

    Implement basic carousel without styling
    Repo: https://github.com/Learus/react-material-ui-carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,43 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import { Button } from 'gatsby-theme-material-ui';
+import Img from "gatsby-image"
+
+import Carousel from "react-material-ui-carousel"
+
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({data: { allFacts: { edges }}}) => {
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+
+      <Carousel>
+        {edges.map(({ node }) => {
+          const { slug } = node.fields
+          const { title, image } = node.frontmatter
+          return (
+            <Paper  eleveation={3} key={node.id}>
+              <Typography variant="h5">
+                {title}
+              </Typography>
+              <CardMedia component={Img} fixed={image.src.childImageSharp.fixed} />
+              <Button color="primary" href={slug}>Learn More</Button>
+            </Paper>
+          )
+        })}
+      </Carousel>
+
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
 
