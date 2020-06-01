3e3c4d5b4e6ab0da1ab7ccea8488bbd5f84392ca
Utkarsh Kumar <kumar375@morris.umn.edu>
2020-06-01 14:00:45 -0500

    Remove carousel package and implementation
    We had a meeting where it was decided that rather than a carousel like this, we want to implement somethings that's more akin to a row of cards navigated via a slider.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,14 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import { Button } from 'gatsby-theme-material-ui';
-import Img from "gatsby-image"
-
-import Carousel from "react-material-ui-carousel"
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import CardMedia from '@material-ui/core/CardMedia';
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const useStyles = makeStyles({
-  carousel: {
-    padding: '3% 15%'
-  },
-  paper: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  }
-})
-
-const IndexPage = ({data: { allFacts: { edges }}}) => {
-  const classes = useStyles();
-  return (
-    <Layout>
-      <SEO title="Home" />
-
-      <Carousel className={classes.carousel}>
-        {edges.map(({ node }) => {
-          const { slug } = node.fields
-          const { title, image } = node.frontmatter
-          return (
-            <Paper className={classes.paper} eleveation={3} key={node.id}>
-              <Typography variant="h5">
-                {title}
-              </Typography>
-              <CardMedia component={Img} fixed={image.src.childImageSharp.fixed} />
-              <Button color="primary" href={slug}>Learn More</Button>
-            </Paper>
-          )
-        })}
-      </Carousel>
-
-      <Link to="/page-2/">Go to page 2</Link>
-    </Layout>
-  )
-}
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
 
 export default IndexPage
 
