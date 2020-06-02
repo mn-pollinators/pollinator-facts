8e768909ba87049c208cfb08eb65701de6909c9f
Utkarsh Kumar <kumar375@morris.umn.edu>
2020-06-01 19:37:23 -0500

    Map all facts on homepage using small fact card

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,22 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import SmallFactCard from "../components/small-fact-card"
 
-const IndexPage = () => (
+
+const IndexPage = ({data: { allFacts: { edges }}}) => (
   <Layout>
     <SEO title="Home" />
+
+    {edges.map(({ node }) => (
+      <SmallFactCard 
+        key={node.id}
+        title={node.frontmatter.title}
+        category={node.frontmatter.category}
+        image={node.frontmatter.image}
+      />
+    ))}
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
@@ -23,6 +35,7 @@ query {
         }
         frontmatter {
           title
+          category
           image {
             src {
               childImageSharp {
@@ -31,6 +44,7 @@ query {
                 }
               }
             }
+            alt
           }
         }
       }
