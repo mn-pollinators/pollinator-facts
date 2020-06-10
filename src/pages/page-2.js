257264ef3e2d4e838b50d6ab4dd3d65f5ad8b39e
Utkarsh Kumar <kumar375@morris.umn.edu>
2020-06-10 13:58:33 -0500

    Change default nav to IconButton. No Functionality

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import { makeStyles } from "@material-ui/core/styles"
+import { IconButton } from 'gatsby-theme-material-ui';
+import NavigateBeforeRoundedIcon from '@material-ui/icons/NavigateBeforeRounded';
+import NavigateNextRoundedIcon from '@material-ui/icons/NavigateNextOutlined';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,50 +12,62 @@ import SmallFactCard from "../components/small-fact-card"
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "../pages/page-2.css"
 
 
 const useStyles = makeStyles({
-  cards: {
+  container: {
+    position: 'relative',
     margin: '50px 0'
   },
   factCard: {
-    minWidth: '190px',
+    maxWidth: '190px',
     margin: '5px',
   }
 });
-function SampleArrows(props) {
-  const { className, style, onClick } = props;
+
+const renderArrows = () => {
   return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", background: "red" }}
-      onClick={onClick}
-    />
+    <div className="slider-arrow">
+      <IconButton 
+        className="arrow-btn prev"
+        onClick= {() => Slider.slickPrev()} 
+      >
+        <NavigateBeforeRoundedIcon />
+      </IconButton>
+      <IconButton 
+        className="arrow-btn next"
+        onClick= {() => Slider.slickNext()} 
+      >
+        <NavigateNextRoundedIcon />
+      </IconButton>
+    </div>
   );
-}
+};
+
 const SecondPage = ({data: { allFacts: { edges }}}) => {
   const classes = useStyles();
+  const navArrows = renderArrows();
   const sliderSettings = {
     dots: true,
+    arrows: false,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 4,
-    nextArrow: <SampleArrows/>,
-    prevArrow: <SampleArrows/>,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToScroll: 3,
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 2,
         }
       },
       {
@@ -67,29 +82,32 @@ const SecondPage = ({data: { allFacts: { edges }}}) => {
   return (
     <Layout>
       <SEO title="Page two" />
-
-      <Slider className={classes.cards} {...sliderSettings}>
-        {edges.map(({ node }) => (
-          <SmallFactCard
-            key={node.id}
-            className={classes.factCard}
-            slug={node.fields.slug}
-            title={node.frontmatter.title}
-            category={node.frontmatter.category}
-            image={node.frontmatter.image}
-          />
-        ))}
-        {edges.map(({ node }) => (
-          <SmallFactCard
-            key={node.id}
-            className={classes.factCard}
-            slug={node.fields.slug}
-            title={node.frontmatter.title}
-            category={node.frontmatter.category}
-            image={node.frontmatter.image}
-          />
-        ))}
-      </Slider>
+      
+      <div  className={classes.container}>
+        {navArrows}
+        <Slider className={classes.slider} {...sliderSettings}>
+          {edges.map(({ node }) => (
+            <SmallFactCard
+              key={node.id}
+              className={classes.factCard}
+              slug={node.fields.slug}
+              title={node.frontmatter.title}
+              category={node.frontmatter.category}
+              image={node.frontmatter.image}
+            />
+          ))}
+          {edges.map(({ node }) => (
+            <SmallFactCard
+              key={node.id}
+              className={classes.factCard}
+              slug={node.fields.slug}
+              title={node.frontmatter.title}
+              category={node.frontmatter.category}
+              image={node.frontmatter.image}
+            />
+          ))}
+        </Slider>
+      </div>
 
       <Link to="/">Go back to the homepage</Link>
     </Layout>
