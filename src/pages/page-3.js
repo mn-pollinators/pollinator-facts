03b411c4fff1df04cd56ee3a0b2e292d4de01549
Nic <robe1606@morris.umn.edu>
2020-06-16 17:28:20 -0500

    Initial Page 3
    Initial commit for page 3, no functionality for linking grid tiles to facts

---
+++ src/pages/page-3.js
@@ -0,0 +1,97 @@
+import React from "react"
+import Img from 'gatsby-image'
+import { Link, graphql } from "gatsby"
+import { makeStyles } from "@material-ui/core/styles"
+import { IconButton } from 'gatsby-theme-material-ui';
+import CardMedia from '@material-ui/core/CardMedia'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import InfoIcon from '@material-ui/icons/Info';
+
+const useStyles = makeStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+    },
+    gridList: {
+      width: 500,
+      height: 500,
+    },
+    gridListTile: {
+      width: 190,
+      height: 'auto',
+    },
+  
+    icon: {
+      color: 'rgba(255, 255, 255, 0.54)',
+    },
+  });
+
+const ThirdPage = ({data: { allFacts: { edges }}}) => {
+  const classes = useStyles();
+  
+  return (
+    <Layout>
+      <SEO title="Page Three" />
+
+      <div className={classes.root}>
+        <GridList className={classes.gridList}>
+          <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+            <ListSubheader component="div">Featured</ListSubheader>
+          </GridListTile>
+          {edges.map(({ node }) => (
+            <GridListTile key={node.id} className={classes.gridListTile}>
+              <CardMedia component={Img} fluid={node.frontmatter.image.src.childImageSharp.fluid} alt={node.frontmatter.title} />
+              <GridListTileBar
+                title={node.frontmatter.title}
+                actionIcon={
+                  <IconButton aria-label={`info about ${node.frontmatter.title}`} className={classes.icon}>
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    </Layout>
+    
+  )
+}
+export default ThirdPage
+
+export const page3Query = graphql`
+query {
+  allFacts: allMarkdownRemark {
+    edges {
+      node {
+        id
+        fields {
+          slug
+        }
+        frontmatter {
+          title
+          category
+          image {
+            src {
+              childImageSharp {
+                fluid(maxWidth: 520) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+            alt
+          }
+        }
+      }
+    }
+  }
+}`
