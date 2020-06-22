ca3fd282885ef4da3f7f1a0f5409a0128182b846
Shawn Reuter <reute078@morris.umn.edu>
2020-06-21 20:58:48 -0500

    Add grid layout to page-4
    Added a grid layout to page-4 using material UI's grid. Also added a page-4 css file to center cards for mobile screens. I am still using the small Fact Cards as the card design.

--- src/pages/page-4.js
+++ src/pages/page-4.js
@@ -2,13 +2,35 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import swiper from "swiper"
+import SmallFactCard from "../components/small-fact-card"
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from "@material-ui/core/styles"
+import "../pages/page-4.css"
 
+const useStyles = makeStyles({
+  factCard: {
+    width: '190px',
+    margin: '10px',
+  }
+});
 
 const  FourthPage = ({data: { allFacts: { edges }}}) => {
+  const classes = useStyles();
   return (
     <Layout>
       <SEO title="Page four" />
+      <Grid className="smallScreen" container flexWrap="wrap" direction="row" spacing={1} >
+      {edges.map(({ node }) => (
+            <SmallFactCard
+              key={node.id}
+              className={classes.factCard}
+              slug={node.fields.slug}
+              title={node.frontmatter.title}
+              category={node.frontmatter.category}
+              image={node.frontmatter.image}
+            />
+          ))}
+      </Grid>
       <Link to="/">Go back to the homepage</Link>
     </Layout>
   )
