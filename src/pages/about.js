a5309461c2a9859dc1b6d322d448715df173a43e
Utkarsh Kumar <kumar375@morris.umn.edu>
2020-12-16 06:45:16 -0600

    Put Contributors in Card and style fundings better

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,13 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { makeStyles } from "@material-ui/core/styles"
-import Img from "gatsby-image"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Typography from '@material-ui/core/Typography'
-import Card from '@material-ui/core/Card'
+import React from "react";
+import { graphql } from "gatsby";
+import { makeStyles } from "@material-ui/core/styles";
+import Img from "gatsby-image";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import Typography from '@material-ui/core/Typography';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 
 import List from '@material-ui/core/List';
@@ -15,9 +17,36 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles({
-  card: {
+  aboutPage: {
+    display: 'grid',
+    placeItems: 'center',
+    gridTemplateRows: '1fr auto',
+    gridTemplateColumns: '100%'
+  },
+  aboutCards: {
+    maxWidth: '800px',
+    margin: '16px',
+    '& .MuiCard-root:not(:last-child)': {
+      marginBottom: '16px'
+    }
+  },
+  enrtf: {
     display: 'flex',
-    flexWrap: 'wrap'
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: '0px -8px -8px',
+    '& > *': {
+      margin: '8px'
+    },
+  },
+  media: {
+    marginTop: 'auto',
+    marginBottom: 'auto'
+  },
+  enrtfText: {
+    minWidth: '60%',
+    flex: 1
   }
 });
 
@@ -33,47 +62,65 @@ const About = ({ data: { enrtfLogo, dynamicUsers } }) => {
   return (
     <Layout>
       <SEO title="About" />
-      {/* Our Mission */}
-      <Typography variant="h2"> Our Mission </Typography>
-      {/* U of M (Not sure if we need this)*/}
+      <div className={classes.aboutPage}>
+        <div className={classes.aboutCards}>
 
-      {/* Acknowledge ENTRF funding */}
-      <Card className={classes.card}>
+          <Card>
+            <CardContent>
+              <Typography variant="h5" component="h2">Contributors</Typography>
 
-        <Typography>
-          Funding for this project was provided by the Minnesota Environment and Natural Resources
-          Trust Fund as recommended by the Legislative-Citizen Commission on Minnesota Resources (LCCMR)
-          The Trust Fund is a permanent fund constitutionally established by the citizens of Minnesota to
-          assist in the protection, conservation, preservation, and enhancement of the state’s air, water, land,
-          fish, wildlife, and other natural resources.
-          Currently 40% of net Minnesota State Lottery proceeds are dedicated to growing the Trust Fund
-          and ensuring future benefits for Minnesota’s environment and natural resources.
-        </Typography>
+              <List component="nav">
+                {dynamicUsers.nodes.map((contributor) => {
+                  const displayName = contributor.name? contributor.name : contributor.login;
+                  return (
+                    <ListItem button component="a" href={contributor.htmlUrl} key={contributor.login}>
+                      <ListItemAvatar>
+                        <Avatar src={contributor.avatarUrl} style={{backgroundColor: randomColor()}}>
+                          {displayName.charAt(0).toUpperCase()}
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary={displayName}/>
+                    </ListItem>
+                  )
+                })}
+              </List>
 
-        <CardMedia
-          component={Img}
-          fixed={enrtfLogo.childImageSharp.fixed}
-          alt="ENRTF Logo"
-          title="ENRTF Logo"
-        />
+            </CardContent>
+          </Card>
 
-      </Card>
+          <Card>
+            <CardContent>
+              <Typography variant="h5" component="h2">Funding</Typography>
 
-      <List component="nav">
-        {dynamicUsers.nodes.map((contributor) => {
-          const displayName = contributor.name? contributor.name : contributor.login;
-          return (
-            <ListItem button component="a" href={contributor.htmlUrl} key={contributor.login}>
-               <ListItemAvatar>
-                <Avatar src={contributor.avatarUrl} style={{backgroundColor: randomColor()}}>
-                  {displayName.charAt(0).toUpperCase()}
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={displayName}/>
-            </ListItem>
-          )
-        })}
-      </List>
+              <div className={classes.enrtf}>
+
+                <CardMedia
+                  className={classes.media}
+                  component={Img}
+                  fixed={enrtfLogo.childImageSharp.fixed}
+                  alt="ENRTF Logo"
+                  title="ENRTF Logo"
+                />
+
+                <div className={classes.enrtfText}>
+                  <Typography paragraph>
+                    Funding for this project was provided by the Minnesota Environment and Natural Resources Trust Fund as recommended
+                    by the <a href="https://www.lccmr.leg.mn/">Legislative-Citizen Commission on Minnesota Resources (LCCMR)</a>
+                  </Typography>
+                  <Typography>
+                    The Trust Fund is a permanent fund constitutionally established by the citizens of Minnesota to assist in the
+                    protection, conservation, preservation, and enhancement of the state’s air, water, land, fish, wildlife, and
+                    other natural resources. Currently 40% of net Minnesota State Lottery proceeds are dedicated to growing
+                    the Trust Fund and ensuring future benefits for Minnesota’s environment and natural resources.
+                  </Typography>
+                </div>
+
+              </div>
+
+            </CardContent>
+          </Card>
+        </div>
+      </div>
 
     </Layout>
   )
@@ -85,7 +132,7 @@ export const aboutQuery = graphql`
 query {
   enrtfLogo: file(relativePath: { eq: "enrtf_logo.png" }) {
     childImageSharp {
-			fixed(width: 100) {
+			fixed(width: 180) {
 				...GatsbyImageSharpFixed_withWebp
 			}
     }
